0c3f0842fbf99acdc95b97bc6c1071194d9ff508
hussein122001 <josefstilt@gmail.com>
2026-01-05 12:02:52 +0300

    Fix ASGI blocking by removing WhiteNoise and fixing middleware/redis

--- mynewsite/asgi.py
+++ mynewsite/asgi.py
@@ -21,21 +21,8 @@
 
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mynewsite.settings")
 
-django_asgi_app = get_asgi_application()
-
-import automation_app.routing  # import AFTER get_asgi_application
-
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            automation_app.routing.websocket_urlpatterns
-        )
-    ),
-})
+application = get_asgi_application()
 
